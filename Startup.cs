7ab81f8e1bb859fa09b2021f6fae34710efd275c
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2020-07-13 21:24:12 +0300

    save all

--- Startup.cs
+++ Startup.cs
@@ -1,19 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using SuLnu.DataAccess;
-using SuLnu.DataAccess.Repositories;
-using SuLnu.DataAccess.Repositories.Base;
-using SuLnu.DataAccess.Interfaces;
-using SuLnu.DataAccess.Interfaces.Base;
-using System.Threading.Tasks;
-using System;
+using Microsoft.Extensions.Hosting;
 
 namespace SuLnu
 {
@@ -25,42 +19,11 @@ namespace SuLnu
         }
 
         public IConfiguration Configuration { get; }
-
         public void ConfigureServices(IServiceCollection services)
         {
-            services.Configure<CookiePolicyOptions>(options =>
-            {
-                options.CheckConsentNeeded = context => true;
-                options.MinimumSameSitePolicy = SameSiteMode.None;
-            });
-
-            services.AddDbContext<SuLnuDBContext>(options =>
-                options.UseSqlServer(
-                    Configuration.GetConnectionString("DefaultConnection")));
-            services.AddDefaultIdentity<IdentityUser>()
-                .AddEntityFrameworkStores<SuLnuDBContext>();
-
-            services.AddScoped<IRepositoryWrapper, RepositoryWrapper>();
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_3_0);
+            services.AddControllersWithViews();
         }
-
-        private async Task CreateAdmin(IServiceProvider serviceProvider)
-        {
-            var userManager = serviceProvider.GetRequiredService<UserManager<IdentityUser>>();
-            var admin = Configuration.GetSection("Admin");
-            var user = new IdentityUser
-            {
-                Email = admin["Email"],
-                UserName = admin["Email"],
-                EmailConfirmed = true,
-            };
-            if (await userManager.FindByEmailAsync(admin["Email"]) == null)
-            {
-                await userManager.CreateAsync(user, admin["Password"]);
-            }
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, IServiceProvider services)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
             {
@@ -71,13 +34,19 @@ namespace SuLnu
                 app.UseExceptionHandler("/Home/Error");
                 app.UseHsts();
             }
-
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-            app.UseCookiePolicy();
 
-            app.UseAuthentication();
-            CreateAdmin(services).Wait();
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
