933ca81857deef1d61e1ede048d6532e411c8d81
unknown <mimi.cry4848@gmail.com>
2020-12-05 17:01:15 +0200

    Implement the posibillity to represent created/active pages

---
+++ SuLnu/Controllers/EventController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using SuLnu.BLL.DTO;
+using SuLnu.BLL.Interfaces;
+using SuLnu.Models;
+
+namespace SuLnu.Controllers
+{
+    public class EventController: Controller
+    {
+        private readonly IEventService _eventService;
+        private readonly IMapper _mapper;
+
+        public EventController(IEventService eventService, IMapper mapper)
+        {
+            _eventService = eventService;
+            _mapper = mapper;
+            var config = new MapperConfiguration(cfg => cfg.CreateMap<EventDTO, EventViewModel>());
+            this._mapper = new Mapper(config);
+        }
+        
+        [HttpGet]
+        public IActionResult AllNews()
+        {
+            var AllEventsDTO = _eventService.GetAll();
+            var AllEvents = _mapper.Map<IEnumerable<EventViewModel>>(AllEventsDTO);
+            return View(AllEvents);
+        }
+    }
+}
---
+++ SuLnu/Models/EventViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace SuLnu.Models
+{
+    public class EventViewModel
+    {
+        public string Name { get; set; }
+        public string FormOfHolding { get; set; }
+        public string Location { get; set; }
+        public string ProfilePhotoFilePath { get; set; }
+        public string EventType { get; set; }
+        public string FacultyName { get; set; }
+    }
+}
