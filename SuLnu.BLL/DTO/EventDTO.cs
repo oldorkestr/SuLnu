d081bb56c553a13aebe0830980d18155551eca9a
unknown <mimi.cry4848@gmail.com>
2020-12-05 18:30:08 +0200

    Implement the possibility to represent created/active events

---
+++ SuLnu.BLL/DTO/EventDTO.cs
@@ -0,0 +1,14 @@
+﻿namespace SuLnu.BLL.DTO
+{
+    public class EventDTO
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string FormOfHolding { get; set; }
+        public string Location { get; set; }
+        public string ProfilePhotoFilePath { get; set; }
+        public string EventType { get; set; }
+        public int FacultyId { get; set; }
+    }
+}
