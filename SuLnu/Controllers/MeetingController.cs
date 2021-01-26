91927ee4860e159a8c81aa8c420d2b1badc99224
Orest Onyshchenko <orest.onyshchenko@gmail.com>
2021-01-26 20:47:31 +0200

    changes

---
+++ SuLnu/Controllers/MeetingController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using SuLnu.BLL.DTO;
+using SuLnu.BLL.Interfaces;
+using SuLnu.BLL.Services;
+using SuLnu.Models;
+
+namespace SuLnu.Controllers
+{
+    public class MeetingController : Controller
+    {
+        private Mapper _mapper;
+
+        //public MeetingController(INewsService newsService, IFacultyService facultyService)
+        //{
+        //    _newsService = newsService;
+        //    _facultyService = facultyService;
+        //    var config = new MapperConfiguration(cfg => cfg.CreateMap<NewsDTO, NewsViewModel>());
+        //    this._mapper = new Mapper(config);
+        //}
+
+        public IActionResult AllMeetings()
+        {
+            return View();
+        }
+    }
+}
---
+++ SuLnu/Controllers/UserController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SuLnu.Models;
+
+namespace SuLnu.Controllers
+{
+    public class UserController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public UserController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult User()
+        {
+            return View();
+        }
+    }
+}
