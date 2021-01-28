a13b0ad1dc0bfab7d2eb514e4dffc17b97018a9d
valentynanochovnayaa <61754279+valentynanochovnayaa@users.noreply.github.com>
2021-01-28 17:32:39 +0200

    Implement the possibility to represent created/active events (#38)
    * Implement the possibility to represent created/active events
    
    * Implement the possibility to represent created/active events
    
    Co-authored-by: ksenyaklakovych <kklakovych@gmail.com>

---
+++ SuLnu.BLL/Interfaces/IEventService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using SuLnu.BLL.DTO;
+
+namespace SuLnu.BLL.Interfaces
+{
+    public interface IEventService
+    {
+        public IEnumerable<EventDTO> GetAll();
+        public void CreateEvents(EventDTO eventDto);
+    }
+}
